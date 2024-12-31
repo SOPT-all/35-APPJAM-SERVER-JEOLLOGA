cab442501647cb34307b94134e55143cf4671912
dajinYang <didekwls0104@naver.com>
2025-01-01 00:47:14 +0900

    [init] initial settings

---
+++ src/main/java/sopt/jeolloga/JeollogaApplication.java
@@ -0,0 +1,13 @@
+package sopt.jeolloga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeollogaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeollogaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sopt/jeolloga/JeollogaApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.jeolloga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeollogaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
